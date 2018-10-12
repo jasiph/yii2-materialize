bd1563a57ff1e00909c7efa35a7c3791ec0ae3cc
J.P <jasiph@ubnntu16.8.8.8.8>
2018-10-13 00:15:48 +0700

    Materialize css added

---
+++ src/MaterializeAsset.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Materialize css file
+ */
+
+namespace jasiph\materialize;
+
+use yii\web\AssetBundle;
+
+class MaterializeAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/materialize/dist';
+    public $css = [
+        'css/materialize.css',
+    ];
+}
